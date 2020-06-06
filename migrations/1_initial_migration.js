9a0a5dc2c22ae1c4640c623753ec2b55cd5f5af3
Hyunwoo Lee <hwlee@cloudbric.com>
2020-06-06 16:15:36 +0900

    Separate migration scripts

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,7 +1,5 @@
 const Migrations = artifacts.require("Migrations");
-const CO2BoardPPM = artifacts.require("./CO2BoardPPM.sol");
 
 module.exports = function(deployer) {
   deployer.deploy(Migrations);
-  deployer.deploy(CO2BoardPPM);
 };
